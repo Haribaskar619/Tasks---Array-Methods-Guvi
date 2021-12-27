67ccc56474038e5cbf77ac6beb803516e7b6b82e
Haribaskar619 <79138852+Haribaskar619@users.noreply.github.com>
2021-12-27 13:57:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ const scores = [
   ];
 
 // Task-1
-  const displayName = scores.map (item =>{
-      return item.name
+  const displayName = scores.map (score =>{
+      return score.name
   })
   console.log("Task-1")
   console.log(displayName);
@@ -46,11 +46,25 @@ console.log(isPassName);
 
 // Task-3 
 
-const isFailName = scores.filter (item =>{
-    return item.marks < 40
-})
+const failed = scores.filter ((score)=> score.marks < 40);
 console.log("Task-3")
-console.log((isFailName));
+console.log(failed.map((score) => score.name));
 
+// Task-4
 
+const displayAvg = scores.map (score =>{
+  return score.marks;
+})
+const avg = displayAvg.reduce((curr,prev) => curr + prev);
+console.log("Task-4")
+console.log(avg/displayAvg.length);
 
+// Task-5
+
+const maxMark = scores.map (score => score.marks);
+// const maxMark1 = scores.reduce((curr,prev)=> (curr > prev) ? curr : prev);
+// console.log(maxMark1);
+const highOfAll = maxMark.reduce((curr,prev)=> (curr > prev) ? curr : prev);
+// const stuName = highOfAll.map((score) => score.name);
+console.log("Task-5") 
+console.log(highOfAll);
